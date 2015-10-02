cf5129e8516531b3cd839a282f5db0495609bf67
Andrew Vagin <avagin@openvz.org>
2015-10-02 20:37:38 +0300

    Create a container for each virtual host
    Currently it's possible to specify a root directory for a container.

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 mod_container:
-	apxs -i -a -c mod_container.c
+	apxs -l ct -l nl-3 -l nl-route-3 -l dbus-1 -i -a -c mod_container.c
--- mod_container.c
+++ mod_container.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 #include "list.h"
+
+#include <libct/libct.h>
+#include <libct/libct-log-levels.h>
 
 #include "apr_hash.h"
 #include "apr_strings.h"
@@ -13,26 +20,114 @@
 #include "http_protocol.h"
 #include "http_request.h"
 
+#include "mpm_common.h"
+
 module AP_MODULE_DECLARE_DATA container_module;
 
 static LIST_HEAD(containers);
+
+static libct_session_t session;
 
 struct container_config {
 	struct list_head node;
 
 	char *root;
+	ct_handler_t ct;
+	ct_process_t pr;
 };
+
+ct_handler_t host_ct;
 
 static int privileges_postconf(apr_pool_t *pconf, apr_pool_t *plog,
                                apr_pool_t *ptemp, server_rec *s)
 {
 	struct container_config *cfg;
+	ct_process_desc_t pd;
+	int fd;
 
-	ap_log_error(APLOG_MARK, APLOG_CRIT, 0, 0, "Start!");
+	ap_log_error(APLOG_MARK, APLOG_DEBUG, 0, 0, "Initialize containers");
+
+	fd = open("/var/log//libct.log", O_WRONLY | O_APPEND | O_CREAT, 0600);
+	if (fd == -1) {
+		ap_log_error(APLOG_MARK, APLOG_CRIT, errno, s, "Unable to open libct log file");
+		return HTTP_INTERNAL_SERVER_ERROR;
+	}
+
+	libct_log_init(fd, LOG_DEBUG);
+
+	session = libct_session_open_local();
+	if (libct_handle_is_err(session))
+		return HTTP_INTERNAL_SERVER_ERROR;
+
+	pd = libct_process_desc_create(session);
+	if (libct_handle_is_err(pd))
+		return HTTP_INTERNAL_SERVER_ERROR;
+	libct_process_desc_set_pdeathsig(pd, SIGKILL);
+
+	{
+		ct_process_t pr;
+		host_ct = libct_container_create(session, "host");
+		if (libct_handle_is_err(host_ct))
+			goto err;
+
+		if (libct_container_set_option(host_ct, LIBCT_OPT_TASKLESS, 0))
+			goto err;
+
+		if (libct_container_set_nsmask(host_ct, CLONE_NEWNS))
+			goto err;
+
+		pr = libct_container_spawn_cb(host_ct, pd, NULL, NULL);
+		if (libct_handle_is_err(pr)) {
+			ap_log_error(APLOG_MARK, APLOG_CRIT, 0, 0, "Unable to start a container\n");
+			goto err;
+		}
+	}
 
 	list_for_each_entry(cfg, &containers, node) {
-		ap_log_error(APLOG_MARK, APLOG_CRIT, 0, 0, "-- %s\n", cfg->root);
+		static int id = 0;
+		char buf[] = "XXXXXXXXXX";
+
+		ap_log_error(APLOG_MARK, APLOG_DEBUG, 0, 0, "Start a container with %s\n", cfg->root);
+
+		snprintf(buf, sizeof(buf), "%d", id++);
+		cfg->ct = libct_container_create(session, buf);
+		if (libct_handle_is_err(cfg->ct))
+			goto err;
+
+		if (libct_container_set_option(cfg->ct, LIBCT_OPT_TASKLESS, 0))
+			goto err;
+
+		if (libct_container_set_nsmask(cfg->ct, CLONE_NEWNS))
+			goto err;
+
+		if (libct_fs_set_root(cfg->ct, cfg->root))
+			goto err;
+
+		cfg->pr = libct_container_spawn_cb(cfg->ct, pd, NULL, NULL);
+		if (libct_handle_is_err(cfg->pr)) {
+			ap_log_error(APLOG_MARK, APLOG_CRIT, 0, 0, "Unable to start a container %s\n", cfg->root);
+			goto err;
+		}
 	}
+	libct_process_desc_destroy(pd);
+	return OK;
+err:
+	list_for_each_entry(cfg, &containers, node) {
+		if (cfg->ct == NULL)
+			continue;
+		libct_container_kill(cfg->ct);
+		libct_container_destroy(cfg->ct);
+	}
+	libct_process_desc_destroy(pd);
+	libct_session_close(session);
+	return HTTP_INTERNAL_SERVER_ERROR;
+}
+
+static int
+unixd_pre_config(apr_pool_t *pconf, apr_pool_t *plog,
+                 apr_pool_t *ptemp)
+{
+	ap_sys_privileges_handlers(1); // FIXME need to work with the unixd module
 	return OK;
 }
 
@@ -42,7 +137,12 @@ static int enter_container_handler (request_rec * r)
 
 	cfg = ap_get_module_config(r->per_dir_config, &container_module);;
 
-	ap_log_error(APLOG_MARK, APLOG_CRIT, 0, 0, "New request: %s\n", cfg->root);
+	ap_log_error(APLOG_MARK, APLOG_DEBUG, 0, 0, "New request: %s\n", cfg->root);
+
+	if (libct_container_switch(cfg->ct)) {
+		ap_log_error(APLOG_MARK, APLOG_CRIT, 0, 0, "Unable to switch into CT\n");
+		return HTTP_INTERNAL_SERVER_ERROR;
+	}
 
 	return DECLINED;
 }
@@ -53,16 +153,23 @@ static int leave_container_handler (request_rec * r)
 
 	cfg = ap_get_module_config(r->per_dir_config, &container_module);;
 
-	ap_log_error(APLOG_MARK, APLOG_CRIT, 0, 0, "Complete request: %s\n", cfg->root);
+	ap_log_error(APLOG_MARK, APLOG_DEBUG, 0, 0, "Complete request: %s\n", cfg->root);
+
+	if (libct_container_switch(host_ct)) {
+		ap_log_error(APLOG_MARK, APLOG_CRIT, 0, 0, "Unable to switch into the host CT\n");
+		return HTTP_INTERNAL_SERVER_ERROR;
+	}
 
 	return DECLINED;
 }
 
 static void register_hooks(apr_pool_t *pool)
 {
+	ap_hook_pre_config(unixd_pre_config,
+                       NULL, NULL, APR_HOOK_FIRST);
 	ap_hook_post_config(privileges_postconf, NULL, NULL,
 						APR_HOOK_REALLY_FIRST);
-	ap_hook_handler(enter_container_handler, NULL, NULL,
+	ap_hook_post_read_request(enter_container_handler, NULL, NULL,
 						APR_HOOK_REALLY_FIRST);
 	ap_hook_log_transaction(leave_container_handler, NULL, NULL,
 						APR_HOOK_LAST);
@@ -82,6 +189,7 @@ static void *container_module_create_dir_config(apr_pool_t * p, char *dirspec)
 	list_add(&cfg->node, &containers);
 
 	cfg->root = NULL;
+	cfg->ct = NULL;
 
 	return cfg;
 }
